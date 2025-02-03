5957b57df835641ec0061b7c637060521963b397
Mahmoud Nabil <126608589+Mmneaa@users.noreply.github.com>
2025-02-03 02:22:17 +0200

    Update Footer.js

--- app/components/Footer/Footer.js
+++ app/components/Footer/Footer.js
@@ -3,7 +3,7 @@ import styles from "./Footer.module.css";
 export default function Footer() {
   return (
     <footer className={styles.footer}>
-      <p>© {new Date().getFullYear()} Profile Grid. All rights reserved.</p>
+      <p>© {new Date().getFullYear()} Mahmoud Nabil. All rights reserved.</p>
     </footer>
   );
 }
